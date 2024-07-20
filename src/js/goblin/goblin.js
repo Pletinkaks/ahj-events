ca66eb43d763d156b36f268ae3570874af6e7859
Ksenia Pletinka <pletinkaksenia@gmail.com>
2024-07-21 00:57:21 +0300

    Initial commit

---
+++ src/js/goblin/goblin.js
@@ -0,0 +1,28 @@
+export default class Goblin {
+    constructor(element) {
+      this._element = element;
+    }
+  
+    toggleActive() {
+  
+      this.hideGoblin();
+  
+      const holes = this._element.querySelectorAll('.hole');
+      const randomHole = holes[Math.floor(Math.random() * holes.length)];
+      randomHole.classList.add('active');
+    }
+  
+    addNewHole() {
+      const item = document.createElement('div');
+      item.classList.add('hole');
+      this._element.insertBefore(item, this._element.querySelector('.hole'));
+  
+    }
+  
+    hideGoblin() {
+      const activeGoblin = this._element.querySelector('.active');
+      if (activeGoblin) {
+        activeGoblin.classList.remove('active');
+      }
+    }
+  }
